ab97323409a48fab69ac67c4eb337fa295ec5b34
Jamie Douglass <jamieraedouglass@gmail.com>
2026-05-14 11:51:57 -0500

    manually construct client ID character by character

--- api/auth.js
+++ api/auth.js
@@ -3,8 +3,11 @@ export default function handler(req, res) {
   const protocol = host.includes('localhost') ? 'http' : 'https';
   const redirectUri = `${protocol}://${host}/api/callback`;
 
+  // Client ID typed manually: capital O, v, 2, 3, l, i, m, v, r, S, g, y, h, D, h, U, h, C, t, k
+  const clientId = ['O','v','2','3','l','i','m','v','r','S','g','y','h','D','h','U','h','C','t','k'].join('');
+
   const params = new URLSearchParams({
-    client_id: 'Ov23limvrSgyhDhUhCtk',
+    client_id: clientId,
     redirect_uri: redirectUri,
     scope: 'repo,user',
     state: Math.random().toString(36).substring(7),
